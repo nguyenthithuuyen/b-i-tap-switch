51b9bcb44b1caf66f998e707fbd0b3556ce77707
nguyenthithuuyen <71063276+nguyenthithuuyen@users.noreply.github.com>
2020-09-16 15:04:19 +0700

    abc

---
+++ js.js
@@ -0,0 +1,17 @@
+function browser() {
+    let browser = parseInt(document.getElementById("browser").textContent);
+
+    switch (browser) {
+        case 'Edge':
+            alert("You've got the Edge!");
+            break;
+        case 'Chrome':
+        case 'Firefox':
+        case 'Safari':
+        case 'Opera':
+            alert('Okay we support these browsers too');
+            break;
+        default:
+            alert('We hope that this page looks ok!');
+    }
+}
